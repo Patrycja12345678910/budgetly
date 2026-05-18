a7d192a9361fb4e929b3c73060a201ac4c84418f
Patrycja Teska <teska.patrycja@wp.pl>
2026-05-18 23:04:09 +0200

    chore: initialize Spring Boot project with PostgreSQL and Docker

---
+++ src/test/kotlin/com/example/budgetly/BudgetlyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.budgetly
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetlyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
